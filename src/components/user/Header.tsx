b258b0330b79420ba2e2984d8a17354b9074ec98
Misthah <kpmisthah6@gmail.com>
2025-04-30 14:18:47 +0530

    add profile

--- src/components/user/Header.tsx
+++ src/components/user/Header.tsx
@@ -1,28 +1,68 @@
-import {FaSignInAlt,FaSignOutAlt,FaUser} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import "../../../public/Header.css"
-const Header = () => {
-  return (
-    <>
-    <header className='header'>
-        <div className='logo'>
-            <Link to='/'>User management</Link>
-        </div>
-        <ul>
-            <li>
-                <Link to='/login'>
-                <FaSignInAlt />Login
-                </Link>
-            </li>
-            <li>
-                <Link to='/register'>
-                <FaUser />Register
-                </Link>
-            </li>
-        </ul>
-    </header>
-    </>
-  )
-}
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import '../../../public/Header.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../app/store';
+import { logout, reset } from '../../features/auth/authSlice';
+import { useState } from 'react';
 
-export default Header
+const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const { user } = useSelector((state: RootState) => state.auth);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate('/login');
+  };
+
+  const toggleDropdown = () => {
+    setShowDropdown((prev) => !prev);
+  };
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">User Management</Link>
+      </div>
+      <ul>
+        {user ? (
+          <div className="user-menu">
+            <div className="user-icon" onClick={toggleDropdown}>
+              <FaUser />
+              <span style={{ marginLeft: '8px' }}>{user.username}</span>
+            </div>
+
+            {showDropdown && (
+              <div className="dropdown">
+                <button onClick={() => alert('Upload feature coming soon')}>
+                  Change Profile Picture
+                </button>
+                <button onClick={onLogout}>
+                  <FaSignOutAlt /> Logout
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
