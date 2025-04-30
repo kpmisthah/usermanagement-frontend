fb2e26c4628d0c68120ff0ca4c5eb269b0081f9c
Misthah <kpmisthah6@gmail.com>
2025-04-30 14:19:12 +0530

    add login reducer

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -21,15 +21,16 @@ interface RegisterPayload {
   email: string;
   password: string;
 }
-
-export const register = createAsyncThunk<
-   User ,
-  RegisterPayload,
-  { rejectValue: string }
->('auth/register', async (userData, { rejectWithValue }) => {
+interface LoginPayload {
+  username: string;
+  password: string;
+}
+//register
+export const register = createAsyncThunk<User ,RegisterPayload,{ rejectValue: string }
+>
+('auth/register', async (userData, { rejectWithValue }) => {
   try {
     const response = await api.post('/auth/signup', userData);
-    localStorage.setItem('token', response.data.access_token); 
     return response.data; 
   } catch (error: any) {
     const message =
@@ -39,7 +40,21 @@ export const register = createAsyncThunk<
     return rejectWithValue(message);
   }
 });
-
+//login
+export const login = createAsyncThunk<User,LoginPayload,{rejectValue:string}
+>
+('/auth/login',async(credentials,{rejectWithValue})=>{
+  try {
+    const response = await api.post('/auth/signin',credentials)
+    console.log(response.data); // <-- See if it has user objec
+    localStorage.setItem('token',response.data.access_token)
+    return response.data.user
+  } catch (error:any) {
+    const message = error.response?.data?.message || 'Login failed';
+    return rejectWithValue(message);
+  }
+})
+//logout
 export const logout = createAsyncThunk(('auth/logout'),async()=>{
  localStorage.removeItem('token')
 })
@@ -77,7 +92,19 @@ const authSlice = createSlice({
         state.isError = true;
         state.message = action.payload as string;
         state.user = null;
-      });
+      })
+      .addCase(logout.fulfilled,(state)=>{
+        state.user = null
+      })
+      .addCase(login.fulfilled,(state,action)=>{
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(login.rejected,(state,action)=>{
+        state.isError = true
+        state.message = action.payload as string
+      })
   },
 });
 
